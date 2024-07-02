12a91943114ca8b321e8e8adde899eeab8357622
stanleyshi42 <stanleyshi42@gmail.com>
2024-07-02 11:54:32 -0700

    added UI for track duration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,8 @@ export class AppComponent {
     'https://api.deezer.com/track/12209331',
     'https://api.deezer.com/track/9997018',
     'https://api.deezer.com/track/2607856',
+    'https://api.deezer.com/track/2603557',
+    'https://api.deezer.com/track/3135554',
   ];
   tracks: any[] = [];
 
@@ -35,6 +37,12 @@ export class AppComponent {
   // Set and play a new track
   setTrack(index: number) {
     let track = this.tracks[index];
+
+    // Check if same track is already playing
+    if (track == this.audioService.currentTrack) {
+      return;
+    }
+    
     this.audioService.setAudio(track);
     this.audioService.playAudio();
   }
